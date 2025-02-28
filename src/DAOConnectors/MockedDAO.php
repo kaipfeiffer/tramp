3edfb44f60c96ef0a709d13d06e1be78b3ca839a
Kai Pfeiffer <kp@paecher.com>
2025-02-28 11:23:06 +0100

    mocked method for table creation

--- src/DAOConnectors/MockedDAO.php
+++ src/DAOConnectors/MockedDAO.php
@@ -15,16 +15,19 @@ class MockedDAO implements DAOConnector
         $this->table = $table;
     }
 
-    public function read(int|null $id = null, int|null $page = null)
+    public function read(int $id = null, int $page = null)
     {
         return $this->table;
     }
 
-    public function read_by(array $query, int|null $page = null) {}
+    public function read_by(array $query, int $page = null) {}
 
     public function create(array $row) {}
 
-    public function create_table(array $row) {}
+    public function create_table(array $data)
+    {
+        return print_r($data, 1);
+    }
 
     public function update(array $row) {}
 
