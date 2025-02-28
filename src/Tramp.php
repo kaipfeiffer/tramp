cfb6a9f85d22c384433382027b7ee48a5b4903ee
Kai Pfeiffer <kp@paecher.com>
2025-02-28 11:24:30 +0100

    test for table creation

--- src/Tramp.php
+++ src/Tramp.php
@@ -11,8 +11,9 @@ class Tramp
 
     public function hello()
     {
-        $this->db   = new MockedDAO('test');
-        $ridings    = new Ridings($this->db);
-        return $ridings->read();
+        $db   = new MockedDAO('test');
+        $ridings    = new Ridings($db);
+        
+        return $db->create_table($ridings->create_table_data());
     }
 }
