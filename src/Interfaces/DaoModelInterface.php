f74159d8a5eec2f82f719173f89308c040097fe8
Kai Pfeiffer <kp@loworx.com>
2025-03-16 18:05:06 +0100

    added model interface

---
+++ src/Interfaces/DaoModelInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kaipfeiffer\Tramp\Interfaces;
+
+interface DaoModelInterface{
+
+    public function read(?int $id, ?int $page = null):?array;
+
+    public function read_by(array $query, ?int $page = null):?array;
+
+    public function create(array $row):?int;
+
+    public function create_table():?int;
+
+    public function update(array $row):?int;
+
+    public function delete(array $row):?bool;
+}
