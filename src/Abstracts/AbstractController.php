dfb947a2c97048743a38a29c167257b9e2612b51
Kai Pfeiffer <kp@loworx.com>
2025-07-25 22:25:53 +0200

    added methods hide_columns, search, get_row_count adn get_primary_key

--- src/Abstracts/AbstractController.php
+++ src/Abstracts/AbstractController.php
@@ -46,13 +46,19 @@ abstract class AbstractController
     abstract protected static function get_model(): DaoModelInterface;
 
 
+
+    /**
+     * PUBLIC METHODS
+     */
+
+
     /**
      * get_editable_columns 
      * 
      * @return  array
      * @since   1.0.0
      */
-    public static function get_editable_columns():array
+    public static function get_editable_columns(): array
     {
         $model = static::get_model();
 
@@ -61,24 +67,105 @@ abstract class AbstractController
 
 
     /**
-     * PUBLIC METHODS
+     * get_primary_key 
+     * 
+     * @return  string
+     * @since   1.0.0
      */
+    public function get_primary_key(): string
+    {
+        $model = static::get_model();
+
+        return $model->get_primary_key();
+    }
+
+
+    /**
+     * get row count
+     * 
+     * get the number of rows in the table
+     * 
+     * @return  int|null
+     * @since   1.0.0
+     */
+    public function get_row_cnt(): ?int
+    {
+        $model = static::get_model();
+
+        return $model->get_row_cnt();
+    }
+
+
+    /**
+     * search
+     * 
+     * @param   string
+     * @param   int
+     * @param   int
+     * @since   1.0.0
+     */
+    public static function search(string $s, ?int $page = null, ?int $per_page = null): ?array
+    {
+        $model = static::get_model();
+        $columns    = $model->get_editable_columns();
+        $columns[$model->get_primary_key()] = null;
+
+        $query = array(
+            'column' => 'familyname',
+            'comparator' => '%like%',
+            'value' => $s
+        );
+        error_log(__CLASS__.'->'.__LINE__.'->'.print_r($query,1));
+        $data       = $model->read_by($query, $page, $per_page);
+
+        return static::hide_columns($data, $columns);
+    }
 
 
     /**
      * read row
      * 
-     * @param   integer
-     * @param   integer
+     * @param   int
+     * @param   int
+     * @param   int
      * @since   1.0.0
      */
-    public static function read(?int $id = null, ?int $page = null):?array
+    public static function read(?int $id = null, ?int $page = null, ?int $per_page = null): ?array
     {
         $model = static::get_model();
-        
-        $data       = $model->read($id, $page);
         $columns    = $model->get_editable_columns();
-        return array_intersect_key($data, $columns);
+        $columns[$model->get_primary_key()] = null;
+
+        $data       = $model->read($id, $page, $per_page);
+
+        return static::hide_columns($data, $columns);
+    }
+
+
+    /**
+     * hide_columns
+     * @param   array $data
+     * @param   array $columns
+     * @return  array|null
+     * @since   1.0.1
+     */
+    protected static function hide_columns(array $data, array $columns):?array
+    {
+        if (array_is_list($data)) {
+            foreach ($data as $index => $details) {
+                if (is_array($details) && is_array($columns)) {
+                    $data[$index]   = array_intersect_key($details, $columns);
+                } else {
+                    $data[$index]   = null;
+                }
+            }
+            return $data;
+        } else {
+            if (!(is_array($data) && is_array($columns))) {
+                return null;
+            }
+            return array_intersect_key($data, $columns);
+        }
     }
 
 
